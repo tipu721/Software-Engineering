814e5ef6b8d257e508e46158024850561696454c
Tipu <tipu@divine-it.net>
2024-04-18 18:25:33 +0600

    delete node in bst first 2 types

--- swe/src/ds/tree/bst/BinarySearchTree.java
+++ swe/src/ds/tree/bst/BinarySearchTree.java
@@ -43,7 +43,51 @@ public class BinarySearchTree {
 
     }
 
+    private Node search(Node node ,int element) {
 
+        if(node == null){
+            return null;
+        }
+        if(node.value == element)
+            return node;
+        if(element < node.value){
+            return search(node.left , element);
+        }
+        else if(element > node.value){
+           return search(node.right, element);
+        }
+        return null;
+    }
+
+    private Node deleteNode(Node root, int element) {
+        if(root == null){
+            System.out.println("element to be deleted not found in BST");
+            return root;
+        }
+        if(root.value == element){
+            if(root.left == null && root.right == null){
+                System.out.println("successfully deleted "+element);
+                return null;
+            }
+            else if(root.left == null){
+                System.out.println("successfully deleted "+element);
+                return root.right;
+
+            }
+            else if(root.right == null){
+                System.out.println("successfully deleted "+element);
+                return root.left;
+            }
+        }
+       if(element < root.value){
+            root.left = deleteNode(root.left, element);
+       }
+       else if(element > root.value){
+            root.right = deleteNode(root.right, element);
+
+       }
+       return root;
+    }
 
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
@@ -54,13 +98,20 @@ public class BinarySearchTree {
 
         bst.traverse(bst.root);
         bst.insert(bst.root, 7);
-        System.out.println("After inser new value");
+        System.out.println("After insert new value");
         bst.traverse(bst.root);
-
-
-
-
-
-
+        System.out.println("Search an element in BST");
+        Node foundNode = bst.search(bst.root,7);
+        if(foundNode == null){
+            System.out.println("Element is not found");
+        }
+        else{
+            System.out.println(foundNode.value + " is found in Bnary Search Tree");
+        }
+        bst.traverse(bst.root);
+        System.out.println("Insert value to be deleted");
+        bst.deleteNode(bst.root, 10);
+        bst.traverse(bst.root);
     }
+
 }
